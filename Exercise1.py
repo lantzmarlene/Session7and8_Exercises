b2cb91cc28467959a6620ca1e03d6a43ee56fc36
Marlene Lantz <lantzmarlene@gmail.com>
2018-10-01 21:33:35 +0200

    Work

--- Exercise1.py
+++ Exercise1.py
@@ -1,7 +1,19 @@
+#If you put summing = 0 before, the computer keeps adding the sums to the previous ones and scaling up, but inside the loop
+#it always sets it to 0 so then whenever you put a bigger number it becomes the new Max
+sMax = -1000
+while True:
+    summing = 0
+    num = input("Give me a number: ")
+    try:
+        num = int(num)
 
-string = input("Please enter a string: ")
-index = len(string)-1
+    except ValueError:
+        print("You have not entered a proper number.")
+        break
 
-while index>=0:
-    print(string[index], end="")
-    index = index - 1
+    summing = summing + num
+
+    if summing > sMax:
+        sMax = summing
+
+    print("The Biggest Sum is: ", sMax)
